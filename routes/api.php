1022e7227d4a70ea91668e349aca6cec8feb6b8a
arta1berisha <ab52558@ubt-uni.net>
2023-03-20 16:12:56 +0100

    bug fix

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use App\Http\Middleware\IfAuthenticated;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-// Route::apiResource('category', CategoryController::class);
+
 
 
 Route::controller(AuthController::class)->group(function () {
@@ -28,11 +28,6 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/logout', 'logout')->middleware(IfAuthenticated::class);
     Route::post('/refresh', 'refresh')->middleware(IfAuthenticated::class);
 });
-
-
-
-
-
 
 Route::group(['middleware' => IfAuthenticated::class], function () {
 
@@ -45,15 +40,6 @@ Route::group(['middleware' => IfAuthenticated::class], function () {
 
     Route::resource('category', CategoryController::class);
 
-    // Route::controller(CategoryController::class)->group(function () {
-    //     Route::post('/category', 'create');
-    //     Route::get('/category', 'index');
-    //     Route::get('/category/{id}', 'show');
-    //     Route::put('/category/{id}', 'update');
-    //     Route::delete('/category/{id}', 'destroy');
-    //     Route::get('/category/{category}/products', 'products');
-    // });
-
     Route::controller(ProductController::class)->group(function () {
         Route::post('/products', 'create');
         Route::get('/products', 'index');
@@ -62,23 +48,3 @@ Route::group(['middleware' => IfAuthenticated::class], function () {
         Route::delete('/products/{id}', 'destroy');
     });
 });
-
-// Route::get('/category   ', [CategoryController::class, 'index'])->middleware(IfAuthenticated::class);
-
-
-// Route::post('/register', [RegisterController::class, 'create']);
-// Route::post('/login', [LoginController::class, 'create']);
-
-// Route::get('users', [LoginController::class, 'index']);
-// Route::get('users/{id}', [LoginController::class, 'show']);
-// Route::put('users/{id}', [LoginController::class, 'update']);
-// Route::delete('users/{id}', [LoginController::class, 'delete']);
-
-// Route::post('products', [ProductController::class, 'create']);
-// Route::get('products', [ProductController::class, 'index']);
-// Route::get('products/{id}', [ProductController::class, 'show']);
-// Route::put('products/{id}', [ProductController::class, 'update']);
-// Route::delete('products/{id}', [ProductController::class, 'delete']);
-
-// Route::post('categories', [CategoryController::class, 'create']);
-// Route::get('categories',)
