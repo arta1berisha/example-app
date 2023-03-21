5506c2bb302b43c22694e2286132666e628d5e99
arta1berisha <ab52558@ubt-uni.net>
2023-03-21 15:34:41 +0100

    User, Products and Categories Resource added

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'category_id' => $this->category_id,
+            'price' => $this->price,
+            'brand' => $this->brand,
+        ];
+    }
+}
