43d6f7f32d6f84fec9c6e1a4de2c91f27234c60a
arta1berisha <ab52558@ubt-uni.net>
2023-03-22 17:19:57 +0100

    Products Form Request added

---
+++ app/Http/Requests/Products/UpdateProductsRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Products;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'string',
+                'max:255',
+            ],
+            'category_id' => [
+                'int',
+            ],
+            'price' => [
+                'string',
+            ],
+            'brand' => [
+                'string',
+                'max:255',
+            ]
+        ];
+    }
+}
+
+
